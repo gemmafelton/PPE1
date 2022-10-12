d192f21a179ded5720d9ed39e92b3b9c5b815028
Gemma <114055021+gemmafelton@users.noreply.github.com>
2022-10-12 11:30:01 +0200

    Update Premier_script.sh

--- Premier_script.sh
+++ Premier_script.sh
@@ -1,7 +1,7 @@
 #usr/bin/bash
 # traitement de l'année 2016, on crée, on écrase le fichier sortie.txt
 echo "pour l'année 2016" > sortie.txt
-grep "Location" 2016*.ann | wc -l >> sorrtie.txt
+grep "Location" 2016*.ann | wc -l >> sortie.txt
 # traitement de l'année 2017, on écrit à la suite du fichier
 echo "pour l'année 2017" >> sortie.txt
 grep "Location" 2017*.ann | wc -l >> sortie.txt
